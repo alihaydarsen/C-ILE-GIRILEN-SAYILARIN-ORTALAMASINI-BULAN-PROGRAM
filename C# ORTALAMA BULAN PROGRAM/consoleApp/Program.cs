43b6f3c33d3fdb2064a8ef0c5c278e32f79b7bb8
alihaydarsen <alihydrsen@gmail.com>
2024-08-06 17:05:57 +0300

    Add files via upload

---
+++ C# ORTALAMA BULAN PROGRAM/consoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//C# İLE GİRİLEN SAYILARIN ORTALAMASINI BULAN PROGRAM
+
+Console.WriteLine("LÜTFEN ORTALAMASINI HESAPLAYACAGIMIZ SAYI ADEDİNİ GİRİNİZ:");
+ int sayıAdedi= Convert.ToInt32(Console.ReadLine());
+ Double toplam= 0;
+ Double yeni;
+  
+ for( int i =1; i <=sayıAdedi; i++){
+    Console.WriteLine($"LÜTFEN ORTALAMASINI HESAPLAYACAGIMIZ {i} .SAYIYI GİRİNİZ" );
+    yeni = Convert.ToInt32(Console.ReadLine());
+    toplam =yeni+toplam;
+ }
+ Console.WriteLine("----------------------------");
+Console.WriteLine("GİRDİGİNİZ SAYILARIN ORTALAMASI: " +toplam/sayıAdedi); 
+
